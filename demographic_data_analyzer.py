ff40cd62dcb527a03411eb62c003e082ecadccf5
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-09 08:23:15 -0500

    Added analysis for the percentage of people with and without advanced education earning more than 50K.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -12,3 +12,10 @@ average_age_men = data[data['sex'] == 'Male']['age'].mean()
 
 # 3. Percentage of people with a Bachelor's degree
 bachelors_deg = (data['education'] == 'Bachelors').mean() * 100
+
+# 4. Percentage of people with advanced education (Bachelors, Masters, Doctorate) earning >50K
+advanced_edu = data['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
+perc_adv_edu_more_than_50K = (data[advanced_edu & (data['salary'] == '>50K')]).shape[0] / data[advanced_edu].shape[0] * 100
+
+# 5. Percentage of people without advanced education earning >50K
+perc_no_adv_edu_more_than_50K = (data[~advanced_edu & (data['salary'] == '>50K')]).shape[0] / data[~advanced_edu].shape[0] * 100
