ec33449aac3b52d02451e5653ab8241ab88c3c9b
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-08 23:14:13 -0500

    Add analysis for race count.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,3 +3,6 @@ import pandas as pd
 # Load the dataset
 file_path = 'adult.data.csv'  # Modify this path as needed
 data = pd.read_csv(file_path)
+
+# 1. Count of people by race
+race_count = data['race'].value_counts()
