87809bd2d19b93690ccd7c5ed2c4f5b5aa37c3cd
Ronnie Nehemia <33788692+HassyMedia@users.noreply.github.com>
2023-12-08 23:16:08 -0500

    Calculate average age of men.

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -6,3 +6,6 @@ data = pd.read_csv(file_path)
 
 # 1. Count of people by race
 race_count = data['race'].value_counts()
+
+# 2. Average age of men
+average_age_men = data[data['sex'] == 'Male']['age'].mean()
